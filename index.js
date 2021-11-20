4cb9cd211b31babed45b808f9ea87660dbf39b23
Coders Key <designer.afride@gmail.com>
2021-11-20 11:30:40 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,81 @@
+const express = require('express')
+const { MongoClient } = require('mongodb');
+require('dotenv').config();
+const cors = require('cors');
+const ObjectId = require('mongodb').ObjectId;
+
+const app = express()
+const port = process.env.PORT || 5000
+
+app.use(cors())
+app.use(express.json())
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.u9lnx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+    try {
+      await client.connect();
+      const database = client.db("easyBuy");
+      const productsCollection = database.collection("products");
+      const ordersCollection = database.collection("orders");
+      
+    //GET API cars
+    app.get('/products', async (req, res) => {
+        const cursor = productsCollection.find({})
+        const products = await cursor.toArray();
+        res.send(products)
+      })
+      
+    // ---------------------------------------------------------
+    // ------------------- ordersCollection  -----------------------------
+    // ---------------------------------------------------------
+
+    // POST API Orders
+    app.post('/orders', async (req, res) => {
+        const order = req.body;
+        const result = await ordersCollection.insertOne(order)
+        res.json(result)
+    })
+
+    //GET API Orders
+    app.get('/orders', async (req, res) => {
+        const cursor = ordersCollection.find({})
+        const orders = await cursor.toArray()
+        res.send(orders)
+    })
+  
+    // GET API Orders Id
+    app.get('/orders/:id', async (req, res) => {
+        const id = req.params.id;
+        // const query = { _id: ObjectId(id)};
+        const query = { _id: id};
+        console.log(query._id)
+        const order = await ordersCollection.findOne(query);
+        res.json(order);
+      })
+
+    // Delete API Orders Id
+    app.delete('/orders/:id', async (req, res) => {
+        const id = req.params.id;
+        // const query = { _id: ObjectId(id)};
+        const query = { _id: id};
+        const order = await ordersCollection.deleteOne(query);
+        res.json(order);
+    })
+   
+  
+    } finally {
+      // await client.close();
+    }
+  }
+  run().catch(console.dir);
+  
+  app.get('/', (req, res) => {
+      res.send('Hello Easy Buy')
+    })
+    
+    app.listen(port, () => {
+      console.log('Running Server Easy Buy', port)
+})
